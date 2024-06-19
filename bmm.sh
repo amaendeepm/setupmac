09f664d7cb20370ce302bfc9351ec43ea425bc80
amaendeepm <amandeepm@gmx.com>
2024-06-19 09:06:21 +0200

    Added brew cask for SQLLite Viewer

--- bmm.sh
+++ bmm.sh
@@ -96,6 +96,7 @@ brew install --cask openvpn-connect
 brew install --cask postman #Postman
 brew install --cask wireshark-chmodbpf #Wireshark capture network
 brew install --cask soapui #SOAPUI for API Integrations testings
+brew install --cask db-browser-for-sqlite
 
 brew install rust-analyzer #After VSCode installed
 
